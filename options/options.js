3c379904c0847c1671af6dd849c6ae3c210012ad
Crowdbotics <team@crowdbotics.com>
2024-02-10 12:43:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rpa-e-hall-pass-46202",
+  url: "https://rpa-e-hall-pass-46202.botics.co",
+  api: "https://rpa-e-hall-pass-46202.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
